a71754d89028df694b03b10f315fa62ba9d50d26
mothepro <4e688d8adf9b8f68774900f9809778ccb7ef857a>
2020-07-07 04:10:00 -0500

    Refactor out lobby and switch

--- index.ts
+++ index.ts
@@ -1,135 +1,178 @@
-import { Spot } from '@mothepro/amazons-engine'
-import storage from 'std:kv-storage'
-import { LitElement, html, customElement, property, css, internalProperty } from 'lit-element'
-import P2P, { State } from '@mothepro/fancy-p2p'
-import type { ProposalEvent, NameChangeEvent } from './src/lobby.js'
-import pkg from './package.json'
+import { customElement, LitElement, html, css, property } from 'lit-element'
+import type { PieceMovedEvent, SpotDestroyedEvent } from '@mothepro/lit-amazons'
+import type { Peer } from '@mothepro/fancy-p2p'
+import Amazons, { Color, Position } from '@mothepro/amazons-engine'
 
-import './src/lobby.js'
-import './src/amazonsWithPeers.js'
-import 'lit-confetti'
+import 'lit-p2p'
+import '@mothepro/lit-amazons'
 
-/** Keys for storing data in kv-storage */
-const enum Keys {
-  /** The name of the user to connect in the lobby as. */
-  NAME = 'name'
+export type GameOverEvent = CustomEvent<Color>
+
+declare global {
+  interface HTMLElementEventMap {
+    'game-over': GameOverEvent
+  }
 }
 
-@customElement('amazons-online')
+// Note: board must be an 8x8 or smaller
+const posToBuf = (...pos: Position[]) => new Uint8Array(
+  pos.map(([x, y]) => x | y << 4))
+
+const bufToPos = (data: ArrayBuffer) => [...new Uint8Array(data)].map(byte => ([
+  byte & 0b0000_1111,
+  byte >> 4,
+] as Position))
+
+@customElement('amazons-with-peers')
 export default class extends LitElement {
-  /** List of STUN servers to broker P2P connections. */
-  @property({ type: Array, reflect: true })
-  stuns!: string[]
 
-  /** Address to the signaling server. */
-  @property({ type: String, reflect: true })
-  signaling!: string
+  protected engine = new Amazons
 
-  /** Version of the signaling server. */
-  @property({ type: String, reflect: true })
-  version!: string
+  @property({ attribute: false })
+  peers?: Peer<ArrayBuffer>[]
 
-  /** Number of times to attempt to make an RTC connection. Defaults to 1 */
-  @property({ type: Number, reflect: true })
-  retries!: number
-
-  /** The number of milliseconds to wait before giving up on the connection. Doesn't give up by default */
-  @property({ type: Number, reflect: true })
-  timeout!: number
-
-  @internalProperty()
-  protected confetti = 0
-
-  private p2p?: P2P<ArrayBuffer>
+  @property({ attribute: false })
+  broadcast?: (data: ArrayBuffer, includeSelf?: boolean) => void
 
   static readonly styles = css`
-    duo-lobby {
-      display: block;
-      margin: 0 auto;
-      max-width: 400px;
-      border: 1px solid #d3d3d3;
-      border-radius: 0.5em;
-    }`
+  :host {
+    display: block;
+    text-align: center;
+  }
+  :host lit-confetti {
+    position: fixed;
+  }
+  :host lit-amazons {
+    border: thin solid black;
+    margin: 0 auto;
+    grid-auto-rows: 1fr;
+    grid-auto-columns: 1fr;
+    width: 100%;
+    max-width: 1000px;
+    max-height: 1000px;
+  }
+  
+  :host ::part(spot) {
+    width: 100%
+  }
+  
+  :host ::part(spot-parity-same) {
+    background-color: lightgrey;
+  }
+  :host ::part(symbol-draggable) {
+    cursor: grab;
+  }
+  :host ::part(symbol-draggable):active {
+    color: red;
+  }
+  :host([dragging]) ::part(symbol-draggable) {
+    cursor: grabbing;
+  }
+  
+  :host :not([ignore])::part(spot-valid) {
+    background-color: yellow;
+  }
+  :host ::part(spot-valid):hover {
+    cursor: pointer;
+    border: thin solid red;
+  }
+  :host ::part(symbol-1):before {
+    content: '♛';
+  }
+  :host ::part(symbol-2):before {
+    content: '♕';
+  }
+  :host ::part(symbol-4):before {
+    content: '💥';
+  }
+
+  /* Symbol Sizing */
+  :host ::part(symbol) {
+    font-size: 1em;
+  }
+  @media (min-width: 576px) { /* bootstrap "sm" */
+    :host ::part(symbol) {
+      font-size: 2em;
+    }
+  }
+  @media (min-width: 768px) { /* bootstrap "md" */
+    :host ::part(symbol) {
+      font-size: 3em;
+    }
+  }
+  @media (min-width: 992px) { /* bootstrap "lg" */
+    :host ::part(symbol) {
+      font-size: 4em;
+    }
+  }
+  @media (min-width: 1200px) { /* bootstrap "xl" */
+    :host ::part(symbol) {
+      font-size: 5em;
+    }
+  }`
+
+  connectedCallback() {
+    console.log('connected', this.peers)
+  }
 
   protected async firstUpdated() {
-    this.connect((await storage.get(Keys.NAME) || '').toString())
+    console.log('updated', this.peers)
+    return
+    this.bindMessages(this.peers![0], 1)
+    this.bindMessages(this.peers![1], 2)
+
+    this.engine.start()
+    for await (const _ of this.engine.stateChange)
+      this.requestUpdate()
+    this.dispatchEvent(new CustomEvent('game-over', { detail: this.engine.waiting }))
   }
 
-  private async connect(name: string) {
-    this.p2p = new P2P(name, {
-      retries: this.retries,
-      timeout: this.timeout,
-      stuns: this.stuns,
-      lobby: `${pkg.name}@${pkg.version}`,
-      server: {
-        address: this.signaling,
-        version: this.version,
-      },
-    })
-
+  private async bindMessages({ message, name, close }: Peer<ArrayBuffer>, color: Color) {
     try {
-      for await (const _ of this.p2p!.stateChange)
-        this.requestUpdate() // Bind state changes to the DOM
-    } catch (err) {
-      console.error(err)
-    }
-    this.requestUpdate()
-  }
+      for await (const data of message) {
+        if (this.engine.current != color)
+          throw Error(`${name} sent ${data} (${data.byteLength} bytes) when it isn't their turn`)
 
-  private nameChange({ detail }: NameChangeEvent) {
-    try {
-      storage.set(Keys.NAME, detail)
-      this.p2p?.leaveLobby()
-      this.connect(detail)
-    } catch (err) {
-      console.error('Unable to reconnect', err)
-    }
-  }
+        switch (data.byteLength) {
+          case 1: // Destroy
+            const [spot] = bufToPos(data)
+            this.engine.destroy(spot)
+            break
 
-  private proposeGroup({ detail }: ProposalEvent) {
-    try {
-      this.p2p!.proposeGroup(detail)
-    } catch (err) {
-      console.error('Proposal failed', err)
-    }
-  }
+          case 2: // Move
+            const [from, to] = bufToPos(data)
+            this.engine.move(from, to)
+            break
 
-  private gameOver() {
-    this.confetti = 150
-    setTimeout(() => this.confetti = 0, 10 * 1000)
-  }
-
-  protected readonly render = () => {
-    if (this.p2p?.stateChange.isAlive)
-      switch (this.p2p!.state) {
-        case State.LOBBY:
-          return html`
-            <duo-lobby
-              .connection=${this.p2p.lobbyConnection}
-              @proposal=${this.proposeGroup}
-              @name-change=${this.nameChange}
-            ></duo-lobby>`
-
-        case State.LOADING:
-          return html`<slot>Loading...</slot>`
-
-        case State.READY:
-          return html`
-            <amazons-with-peers
-              .broadcast=${this.p2p.broadcast}
-              .peers=${{
-                [Spot.BLACK]: this.p2p.peers[0],
-                [Spot.WHITE]: this.p2p.peers[1],
-              }}
-              @game-over=${this.gameOver}
-            ></amazons-with-peers>
-            <lit-confetti
-              part="confetti"
-              gravity=1
-              count=${this.confetti}
-            ></lit-confetti>`
+          default:
+            throw Error(`Only expected 1 or 2 bytes, but ${name} sent ${data} (${data.byteLength} bytes)`)
+        }
       }
-
-    return html`<slot>Offline</slot>`
+    } catch (err) {
+      console.error('Lost Connection with', name, err)
+    }
   }
+
+  protected readonly render = () => this.peers && this.broadcast && html`
+    <h1 part="title is-${this.engine.stateChange.isAlive ? 'ongoing' : 'over'}">${this.engine.stateChange.isAlive
+      ? this.peers[this.engine.current].isYou
+        ? 'Your turn'
+        : `${this.peers[this.engine.current].name}'s turn`
+      : this.peers[this.engine.waiting].isYou
+        ? 'You Win!'
+        : `${this.peers[this.engine.waiting].name} Wins!`
+    }</h1>
+    <lit-amazons
+      part="game"
+      ?ignore=${!this.peers[this.engine.current].isYou}
+      state=${this.engine.state}
+      current=${this.engine.current}
+      .destructible=${this.engine.destructible}
+      .pieces=${this.engine.pieces}
+      .board=${this.engine.board}
+      @piece-moved=${({ detail: { from, to } }: PieceMovedEvent) => this.broadcast!(posToBuf(from, to))}
+      @spot-destroyed=${({ detail: spot }: SpotDestroyedEvent) => this.broadcast!(posToBuf(spot))}
+      @piece-picked=${() => this.setAttribute('dragging', '')}
+      @piece-let-go=${() => this.removeAttribute('dragging')}
+    ></lit-amazons>`
 }
